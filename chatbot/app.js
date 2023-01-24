b65b641fd8857074f9988c529a8f24026793721d
ASHR12 <shahidshrivastava.01@gmail.com>
2023-01-24 11:47:55 +0530

    Completed CHAT BOT using OPEN AI API

---
+++ chatbot/app.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("chat-form");
+const input = document.getElementById("chat-input");
+const messages = document.getElementById("chat-messages");
+const apiKey = "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";
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const message = input.value;
+  input.value = "";
+
+  messages.innerHTML += `<div class="message user-message">
+  <img src="./icons/user.png" alt="user icon"> <span>${message}</span>
+  </div>`;
+
+  // Use axios library to make a POST request to the OpenAI API
+  const response = await axios.post(
+    "https://api.openai.com/v1/completions",
+    {
+      prompt: message,
+      model: "text-davinci-003",
+      temperature: 0,
+      max_tokens: 1000,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+    }
+  );
+  const chatbotResponse = response.data.choices[0].text;
+
+  messages.innerHTML += `<div class="message bot-message">
+ <img src="./icons/chatbot.png" alt="bot icon"> <span>${chatbotResponse}</span>
+  </div>`;
+});
